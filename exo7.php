bc8588df41e1ad60fb8e1a86bfa98df859bc0e6e
Oumyf <falladiaraoumy@gmail.com>
2024-02-21 12:47:58 +0000

    exo de concaténation

---
+++ exo7.php
@@ -0,0 +1,23 @@
+<?php
+
+$tab = [ 'bonjour' , 'tout le monde' , "je vais vous enseigner l'anglais"] ; 
+
+for ($i=0; $i < count($tab); $i++) { 
+    $chaine = $tab [$i] . " ";
+
+    echo $chaine ;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
